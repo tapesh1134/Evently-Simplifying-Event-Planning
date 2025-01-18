f36a74142fcf183ab157f3ff885e7e4d2b39157d
tapesh1134 <tapeshs1134@gmail.com>
2025-01-18 20:18:39 +0530

    fixing redux in frontend

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
+import eventReducer from "./slices/eventSlice"
 
 export const store = configureStore({
     reducer: {
-        user: userReducer
+        user: userReducer,
+        event: eventReducer,
     },
 });
