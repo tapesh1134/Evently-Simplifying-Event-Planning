7d8292a2bd2f69aa877d9ff2e7ab021a27e2b677
tapesh1134 <tapeshs1134@gmail.com>
2025-02-02 18:50:17 +0530

    fixing

--- backend/router/commentRoutes.js
+++ backend/router/commentRoutes.js
@@ -3,10 +3,7 @@ import { addComment, getComments } from "../controllers/commentController.js";
 
 const router = express.Router();
 
-// Post a comment
 router.post("/", addComment);
-
-// Get comments for a specific post
 router.get("/:postId", getComments);
 
 export default router;
--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,9 +1,4 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:5173"; // Change URL if needed
-
-const socket = io(SOCKET_URL, {
-  withCredentials: true,
-});
-
+const socket = io("http://localhost:8000");
 export default socket;
